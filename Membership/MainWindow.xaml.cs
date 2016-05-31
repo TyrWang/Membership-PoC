36d12904d07bc630ea2b7f947fb68346d70952c2
TyrWang <tyrwang@kkbox.com>
2016-05-31 11:18:40 +0800

    first push

---
+++ Membership/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Membership
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IMember user = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            user = new PremiumMember();
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            user = new NormalMember();
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            if (user == null)
+                MessageBox.Show("not a member");
+
+            MessageBox.Show(user.Play());
+        }
+    }
+}
---
+++ Membership/Member/PremiumMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membership
+{
+    class PremiumMember : IMember
+    {
+        public string Play()
+        {
+            return "I'm at a payphone trying to call home~";
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
